75a1a1a0259d5d541056bf6d7d55ae2afcb6615e
Rakib Ansari <rakib.ai.ds@gmail.com>
2025-12-02 09:43:02 +0530

    a

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 import { metaImagesPlugin } from "./vite-plugin-meta-images";
 
 export default defineConfig({
+  base: '/deploy_hero/',
   plugins: [
     react(),
     runtimeErrorOverlay(),
@@ -25,9 +26,7 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      "@": path.resolve(import.meta.dirname, "client", "src"),
-      "@shared": path.resolve(import.meta.dirname, "shared"),
-      "@assets": path.resolve(import.meta.dirname, "attached_assets"),
+      "@": path.resolve(import.meta.dirname, "src"),
     },
   },
   css: {
@@ -35,9 +34,8 @@ export default defineConfig({
       plugins: [],
     },
   },
-  root: path.resolve(import.meta.dirname, "client"),
   build: {
-    outDir: path.resolve(import.meta.dirname, "dist/public"),
+    outDir: path.resolve(import.meta.dirname, "dist"),
     emptyOutDir: true,
   },
   server: {
